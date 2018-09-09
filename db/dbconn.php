237f31e4003ea14e39de7b5899e711cf99703af9
vimukthisineth <vimukthisineth@gmail.com>
2018-09-09 08:33:29 +0530

    spelling corrected

--- db/dbconn.php
+++ db/dbconn.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "byensell";
+$dbname = "buynsell";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
